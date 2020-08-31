c3c140f17c3df2ce08ccf8e5446bc4f360922884
William Killian <william.killian@gmail.com>
2020-08-31 17:53:44 -0400

    Add emote reactor

--- bot.py
+++ bot.py
@@ -8,4 +8,5 @@ description = '''the MUCS Discord Bot'''
 if __name__ == '__main__':
 	bot = commands.Bot(command_prefix='!')
 	bot.load_extension('cogs.football_toss')
+	bot.load_extension('cogs.emote_reactor')
 	bot.run(os.environ['DISCORD_TOKEN'])
---
+++ cogs/emote_reactor.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+
+class EmoteReactorCog(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	lookup = {
+		'a': ['🇦', '🅰️'],
+		'b': ['🇧', '🅱️'],
+		'c': ['🇨'],
+		'd': ['🇩'],
+		'e': ['🇪'],
+		'f': ['🇫'],
+		'g': ['🇬'],
+		'h': ['🇭'],
+		'i': ['🇮'],
+		'j': ['🇯'],
+		'k': ['🇰'],
+		'l': ['🇱'],
+		'm': ['🇲', 'Ⓜ️'],
+		'n': ['🇳'],
+		'o': ['🇴', '🅾️'],
+		'p': ['🇵', '🅿️'],
+		'q': ['🇶'],
+		'r': ['🇷'],
+		's': ['🇸'],
+		't': ['🇹'],
+		'u': ['🇺'],
+		'v': ['🇻'],
+		'w': ['🇼'],
+		'x': ['🇽'],
+		'y': ['🇾'],
+		'z': ['🇿'],
+		'1': ['1️⃣'],
+		'2': ['2️⃣'],
+		'3': ['3️⃣'],
+		'4': ['4️⃣'],
+		'5': ['5️⃣'],
+		'6': ['6️⃣'],
+		'7': ['7️⃣'],
+		'8': ['8️⃣'],
+		'9': ['9️⃣'],
+		'0': ['0️⃣'],
+		}
+	
+	@commands.command(
+		name='emotereact',
+		description='Add emote flair to someone\'s post',
+		usage='<message id> <string>'
+	)
+	async def emotereact(self, ctx, message_id: int, react_string: str):
+		await ctx.message.delete()
+		msg = await ctx.channel.fetch_message(message_id)
+		counts = {}
+		emotes = []
+		try:
+			for c in react_string:
+				if not c in counts:
+					counts[c] = 0
+				else:
+					counts[c] += 1
+				e = EmoteReactorCog.lookup[c][counts[c]]
+				emotes.append(e)
+			for e in emotes:
+				await msg.add_reaction(e)
+		except:
+			await ctx.author.send("the reaction string cannot be represented using the characters specified", delete_after=30.0)
+		return
+
+def setup(bot):
+	bot.add_cog(EmoteReactorCog(bot))

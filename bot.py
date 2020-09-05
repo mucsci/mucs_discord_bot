1b6641e80446680611c48886eaf9fadadf83d66e
Alex Stone <amstone2@millersville.edu>
2020-09-05 16:53:31 -0400

    Add CDO commands

--- bot.py
+++ bot.py
@@ -8,5 +8,6 @@ description = '''the MUCS Discord Bot'''
 if __name__ == '__main__':
 	bot = commands.Bot(command_prefix='!')
 	bot.load_extension('cogs.football_toss')
+	bot.load_extension('cogs.cdo_bot')
 	bot.load_extension('cogs.emote_reactor')
 	bot.run(os.environ['DISCORD_TOKEN'])

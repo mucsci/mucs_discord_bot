7d12962bff0d13473ff04b394e77a0a29faaf254
rickolas <54609312+ricknyan@users.noreply.github.com>
2020-09-17 19:45:05 -0400

    Most Voted (#6)
    The bot will now display the most voted choice when the poll timer has ended.

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,5 +1,4 @@
 import re
-import random
 import asyncio
 import discord
 from discord.ext import commands
@@ -11,40 +10,39 @@ class PollCog(commands.Cog):
     @commands.command(
         name = 'pollhelp',
         description = 'Explains how the poll command works.',
-        usage=''
     )
     async def pollhelp(self, ctx):
-        await ctx.message.delete()
-        
         embed = discord.Embed(
-            title = 'Poll Help',
-            description = 'This command allows you to create a very simple poll.\n' 
+            title='Poll Help',
+            description='This command allows you to create a very simple poll.\n' 
                 + 'The poll question should be in quotes, followed by your\ncomma seperated choices. '
                 + 'The last argument after the | is optional.\nThis allows you to set a time limit (seconds) on your poll.\n'
                 + 'The poll has a default time limit of 2 minutes if no time is set.',
-            colour = 0x62738d)
+            colour=0x62738d)
         embed.add_field(name='Example Syntax', value='!poll "<question>" <choice1>, <choice2>, .. <choiceN> | <seconds>')
-
         await ctx.send(embed=embed)
 
     @commands.command(
-        name = 'poll',
-        description = 'Creates a simple poll, check out !pollhelp for more details',
-        usage = '"<question>" <choice1>, <choice2>, .. <choiceN>'
+        name='poll',
+        description='Creates a simple poll, check out !pollhelp for more details',
+        usage= '"<question>" <choice1>, <choice2>, .. <choiceN>'
     )
     async def poll(self, ctx):
         await ctx.message.delete()
 
         question = re.findall('"([^"]*)', ctx.message.clean_content)
         if not question:
-            await ctx.send(f"{ctx.message.author.mention}, for syntax check out !help poll")
+            await ctx.send(f"For syntax check out !help poll, {ctx.message.author.mention}")
             return
-        choices = question[1]
+        choices = question[1].strip()
         if not choices:
-            await ctx.send(f"{ctx.message.author.mention}, for syntax check out !pollhelp")
+            await ctx.send(f"For syntax check out !help poll, {ctx.message.author.mention}")
             return
-        if not choices.find('|') == -1:
+        if choices.find('|') > -1:
             choices = choices.split('|')
+            if not choices[1].strip().isdigit():
+                await ctx.send(f"The timer argument must be an integer, {ctx.message.author.mention}")
+                return
             self.timer = int(choices[1])
             choices = choices[0]
         else:
@@ -52,18 +50,27 @@ class PollCog(commands.Cog):
 
         poll_choices = [(chr(0x1f1e6 + i), c) for i, c in enumerate(choices.split(',')[0:])]
         poll_embed = discord.Embed(
-            title = "**" + question[0] + "**",
-            description = "\n".join(f"{key} : {c}" for key, c in poll_choices),
-            colour = 0x83bae3
+            title="**" + question[0] + "** :bar_chart:",
+            description="\n".join(f"{key} : {c}" for key, c in poll_choices),
+            colour=0x83bae3
         )
 
         poll = await ctx.send(embed=poll_embed)
         for emoji, _ in poll_choices:
-            await poll.add_reaction(emoji)
-
+            await poll.add_reaction(emoji) 
         await asyncio.sleep(self.timer)
-        await poll.delete()
+
+        poll_message = await ctx.fetch_message(poll.id)
+        reactions = [(poll_choices[i][1], reaction.count) for i, reaction in enumerate(poll_message.reactions)]
+        reactions.sort(key=lambda x:x[1])
+
+        end_poll_embed = discord.Embed(
+            title="**" + question[0] + "** :bar_chart:",
+            description=(f"\nThe most voted choice was **{reactions[-1][0]}**"),
+            colour=0x83bae3
+        )
+        await poll_message.clear_reactions()
+        await poll_message.edit(content=(f"{ctx.message.author.mention}"), embed=end_poll_embed)
 
 def setup(bot):
     bot.add_cog(PollCog(bot))
-

2c45b5aa85f1bffecc9d93f286085961aa9c695c
John <54130978+JohnHynes@users.noreply.github.com>
2022-10-09 11:03:21 -0400

    Mod toss (#14)

--- cogs/football_toss.py
+++ cogs/football_toss.py
@@ -74,7 +74,7 @@ class FootballTossCog(commands.Cog):
 				await add_one ('total_fails')
 			else:
 				count = await self._get_count(ctx, receiver)
-				if thrower.top_role.id not in privileged_throwers:
+				if receiver.top_role.id not in privileged_throwers:
 					await remove_role(receiver, count)
 					await add_role(receiver, count + 1)
 				await ctx.send(f'{thrower.mention} {action}s a {item} at {receiver.mention}')

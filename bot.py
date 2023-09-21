3c24ee379e0702fde8aeb3df3caf0981913b44aa
Sean Malloy <31411598+sfmalloy@users.noreply.github.com>
2023-09-20 20:27:27 -0400

    Add football toss slash commands (#15)

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import random
 import os
 import aiohttp 
+from utils.constants import Constants
 
-guild_id = 736215823034744895
 description = '''the MUCS Discord Bot'''
 
 # Loading our cogs, iterating through each name in folder named cogs
@@ -15,12 +15,13 @@ async def load_extensions():
 
 class Bot(commands.Bot): # New bot class extending commands.Bot
         def __init__(self): # Initializing bot
-                super().__init__(command_prefix='!', intents=discord.Intents.all(), application_id=748146175626182667)
+                super().__init__(command_prefix='!', intents=discord.Intents.all(), application_id=Constants.APPLICATION_ID)
 
         async def setup_hook(self): # Setup hook for cogs
                 self.session = aiohttp.ClientSession() # New aiohttp session
                 await load_extensions() # Loading cogs
-                await bot.tree.sync(guild=discord.Object(id=guild_id)) #Sync commands
+                synced = await self.tree.sync(guild=discord.Object(id=Constants.GUILD_ID)) #Sync commands
+                print(f'Synced {len(synced)} commands')
 
         async def close(self): # Closing the session so it won't error
                 await super().close()
@@ -29,7 +30,6 @@ class Bot(commands.Bot): # New bot class extending commands.Bot
         async def on_ready(self): # Inform that it's ready
                 await self.wait_until_ready()
                 print(f"Logged in as {self.user}.")
-
 
 if __name__ == '__main__':
         bot = Bot() # Calling the class

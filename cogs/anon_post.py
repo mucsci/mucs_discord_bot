6efa75b7483438429bda7266be02a7b699121705
Will Killian <william.killian@outlook.com>
2024-05-05 15:53:04 -0400

    Add more channels for anon posting; logging to moderator-only
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- cogs/anon_post.py
+++ cogs/anon_post.py
@@ -10,16 +10,21 @@ class AnonPostCog(commands.Cog):
 	
 	@app_commands.command(
 		name='anon',
-		description='Anonymously post to a course channel',
+		description='Anonymously post to a supported channel',
 	)
 	async def anon(self, interaction: discord.Interaction, message: str):
 		try:
-			_ = int(str(interaction.channel.name))
-			interaction.user = self.bot.user
-			await interaction.response.send_message("Posted anonymously", delete_after=15.0, ephemeral=True)
-			await interaction.channel.send('**[anonymous post]** ' + message)
+			category_name = interaction.channel.category.name.lower()
 		except:
-			await interaction.response.send_message("You can only post anonymously in course channels", delete_after=15.0, ephemeral=True)
+			category_name = None
+		CATEGORIES = ["subjects", "csci courses", "language help", "resources", "help"]
+		if category_name in CATEGORIES:
+			await interaction.response.send_message("Posted anonymously", delete_after=15.0, ephemeral=True)
+			mod_channel = await self.bot.fetch_channel(Constants.MOD_ONLY_CHANNEL_ID)
+			await interaction.channel.send('**[anonymous]** ' + message)
+			await mod_channel.send(f"{interaction.user.name} sent an anonymous message in {interaction.channel.name}: {message}")
+		else:
+			await interaction.response.send_message(f"You can only post anonymously in the following categories: {', '.join(CATEGORIES)}", delete_after=15.0, ephemeral=True)
 
 async def setup(bot):
 	await bot.add_cog(AnonPostCog(bot), guilds=[discord.Object(id=Constants.GUILD_ID)])
